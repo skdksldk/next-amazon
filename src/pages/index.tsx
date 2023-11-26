4769a1c03c294dce4eda91229189b79459c6d0f4
skdksldk <tnwns1485@naver.com>
2023-11-27 02:35:08 +0900

    Add Header

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import BottomHeader from "@/components/header/BottomHeader";
+import Header from "@/components/header/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <BottomHeader />
+    </main>
+  )
+}
