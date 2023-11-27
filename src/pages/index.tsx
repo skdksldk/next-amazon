aedfca57ca69891093de9a38c0b046fa645eaf56
skdksldk <tnwns1485@naver.com>
2023-11-27 12:01:00 +0900

    Add Products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Banner from "@/components/Banner"
+import Products from "@/components/Products"
+import { ProductProps } from "../../type";
 
-export default function Home() {
+interface Props {
+  productData: ProductProps;
+}
+
+export default function Home({ productData }: Props) {
+  console.log(productData);
   return (
     <main>
       <div className="max-w-screen-2x1 mx-auto">
         <Banner />
+        <div className="relative md:-mt020 lgl:-mt-32 xl:-mt-6- z-20 mb-10">
+          <Products productData={productData} />
+        </div>
       </div>
     </main>
   )
 }
+
+// SSR for data fetching
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return { props: { productData } };
+};
