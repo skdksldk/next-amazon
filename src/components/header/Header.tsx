84db98b7ea1f3292d00ca6f3cff7269383169427
skdksldk <tnwns1485@naver.com>
2023-11-27 10:38:45 +0900

    Add RootLayout Component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,17 +4,19 @@ import cartIcon from "../../images/cartIcon.png";
 import { BiCaretDown } from "react-icons/bi";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlLocationPin } from "react-icons/sl";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
          <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
           {/* logo */}
-          <div
+          <Link
+            href={"/"}
             className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]"
           >
             <Image className="w-28 object-cover mt-1" src={logo} alt="logoImg" />
-          </div>
+          </Link>
           {/* delivery */}
           <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
             <SlLocationPin />
@@ -54,7 +56,8 @@ const Header = () => {
             <p className="text-white font-bold">& Favorite</p>
           </div>
           {/* cart */}
-          <div
+          <Link
+            href={"/cart"}
             className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative"
           >
             <Image
@@ -66,7 +69,7 @@ const Header = () => {
             <span className="absolute text-amazon_yellow text-sm top-2 left-[29px] font-semibold">
               0
             </span>
-          </div>
+          </Link>
         </div>
       </div>
     )
