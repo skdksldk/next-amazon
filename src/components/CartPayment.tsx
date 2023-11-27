16ee0c9aa117dd3be482b7b6ab25270f427b2a47
skdksldk <tnwns1485@naver.com>
2023-11-28 02:41:13 +0900

    Stripe Checkout

--- src/components/CartPayment.tsx
+++ src/components/CartPayment.tsx
@@ -3,6 +3,8 @@ import FormattedPrice from "./FormattedPrice";
 import { useDispatch, useSelector } from "react-redux";
 import { StateProps, StoreProduct } from "../../type";
 import { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useSession } from "next-auth/react";
 
 const CartPayment = () => {
   const { productData, userInfo } = useSelector(
@@ -17,6 +19,32 @@ const CartPayment = () => {
     });
     setTotalAmount(amt);
   }, [productData]);
+   // Striep payment
+   const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  );
+  const { data: session } = useSession();
+
+  const handleCheckout = async () => {
+    const stripe = await stripePromise;
+
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ items: productData, email: session?.user?.email }),
+    });
+    const checkoutSession = await response.json();
+
+    // Redirecting user/customer to Stripe Checkout
+    const result: any = await stripe?.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+    if (result.error) {
+      alert(result?.error.message);
+    }
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -38,6 +66,7 @@ const CartPayment = () => {
       {userInfo ? (
         <div className="flex flex-col items-center">
           <button
+            onClick={handleCheckout}
             className="w-full h-10 text-sm font-semibold bg-amazon_blue text-white rounded-lg hover:bg-amazon_yellow hover:text-black duration-300"
           >
             Proceed to Buy
